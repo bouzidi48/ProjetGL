9923af7c8d24c7dbc71ab4118ff3eafb9473a8c1
othykb <o.kellabennani@esisa.ac.ma>
2025-01-10 10:03:11 +0100

    test data

--- ProjetGL/Data/Data.cs
+++ ProjetGL/Data/Data.cs
@@ -2,14 +2,14 @@
 
 namespace ProjetGL.Data
 {
-    public class Data
+    public class Data : IData
     {
         SqlConnection connection = new SqlConnection();
         SqlCommand command = new SqlCommand();
 
         public Data()
         {
-            connection.ConnectionString = $@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDGL;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            connection.ConnectionString = $@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDG;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
             connection.Open();
             command.Connection = connection;
         }
---
+++ ProjetGL/Data/IData.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetGL.Data
+{
+    public interface IData
+    {
+        void Add();
+        void Delete();
+        bool Exist();
+        void Find(string login);
+        void GetAccounts();
+        void Update();
+    }
+}
