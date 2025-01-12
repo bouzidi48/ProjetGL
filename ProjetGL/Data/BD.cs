942edf8d46231bc48ed7f4bbcad7ff47d3d44f6f
OthmaneBk <okebennani@gmail.com>
2025-01-12 14:26:24 +0100

    XCFDQ

---
+++ ProjetGL/Data/BD.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ProjetGL.Data
+{
+    public class BD
+    {
+        SqlConnection connection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+
+        public BD()
+        {
+            connection.ConnectionString = $@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDGL;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            connection.Open();
+            command.Connection = connection;
+        }
+    }
+}
--- ProjetGL/Data/Data.cs
+++ ProjetGL/Data/Data.cs
@@ -4,14 +4,10 @@ namespace ProjetGL.Data
 {
     public class Data : IData
     {
-        SqlConnection connection = new SqlConnection();
-        SqlCommand command = new SqlCommand();
 
         public Data()
         {
-            connection.ConnectionString = $@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDGL;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-            connection.Open();
-            command.Connection = connection;
+            BD bD = new BD();
         }
 
 
