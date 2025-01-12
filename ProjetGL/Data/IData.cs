bdad46f67172d851243c2108973b8f292a69f536
bouzidi48 <mohemmedbouzidi16@gmail.com>
2025-01-12 13:09:01 +0100

    fdsfd

--- ProjetGL/Data/IData.cs
+++ ProjetGL/Data/IData.cs
@@ -5,5 +5,14 @@ namespace ProjetGL.Data
 {
     public interface IData
     {
-    }
+		void Add(Account account);
+		void Del(string login);
+		bool Exist(string login);
+		Account Find(string login);
+		List<Account> GetAccounts();
+		void Update(string login, Account newAccount);
+
+
+
+	}
 }
