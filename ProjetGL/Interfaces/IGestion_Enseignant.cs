c3163194da10a9cf149c3050deed32e84a12540d
nabilqasmii <Nabil.qasmi1@gmail.com>
2025-01-31 22:31:43 +0100

    hghghghghg

---
+++ ProjetGL/Interfaces/IGestion_Enseignant.cs
@@ -0,0 +1,11 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+	public interface IGestion_Enseignant
+	{
+		bool ExistEnseignant(int id);
+		Enseignant FindEnseignant(int id);
+		List<Enseignant> GeEnseignants();
+	}
+}
