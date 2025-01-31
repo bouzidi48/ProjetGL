7295d3540597e90a7ed7ca5a644ab82248c4d881
nabilqasmii <Nabil.qasmi1@gmail.com>
2025-01-31 22:31:09 +0100

    ghghg

---
+++ ProjetGL/Interfaces/IDepartementManager.cs
@@ -0,0 +1,12 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+	public interface IDepartementManager
+	{
+		bool ExistDepartement(int id);
+		Departement FindDepartement(int id);
+		List<Departement> GetDepartements();
+
+	}
+}
---
+++ ProjetGL/Interfaces/IEnseignantManager.cs
@@ -0,0 +1,11 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+	public interface IEnseignantManager
+	{
+		bool ExistEnseignant(int id);
+		Enseignant FindEnseignant(int id);
+		List<Enseignant> GeEnseignants();
+	}
+}
