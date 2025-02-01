692750639c718cb748b09ef4eb913560ccc9e875
Ilyass <i.haddad@esisa.ac.ma>
2025-02-01 18:02:55 +0100

    hh

---
+++ ProjetGL/Interfaces/IGestion_Proposition.cs
@@ -0,0 +1,11 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+    public interface IGestion_Proposition
+    {
+        void AddProposition(Proposition proposition);
+        Proposition FindProposition(int id);
+        List<Proposition> GetAllPropositions();
+    }
+}
---
+++ ProjetGL/Interfaces/IGestion_Ressource.cs
@@ -0,0 +1,14 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+	public interface IGestion_Ressource
+	{
+		void AffecterRessource(AffectationRessource affectation);
+		void AjouterRessource(Ressource ressource);
+		List<Departement> GetAllDepartements();
+		List<Enseignant> GetAllEnseignants();
+		List<Ressource> GetAllRessources();
+		void SupprimerRessource(int ressourceId);
+	}
+}
