18feaad0391545794b6a88bd382ec77ed5bfade2
OussamaAatany <o.aatany@esisa.ac.ma>
2025-02-01 17:55:47 +0100

    ff

--- ProjetGL/Data/Gestion_Account.cs
+++ ProjetGL/Data/Gestion_Account.cs
@@ -1,111 +1,111 @@
 ﻿using Microsoft.Data.SqlClient;
+using ProjetGL.Models;
 using ProjetGL.GlobalServices;
 using ProjetGL.Interfaces;
-using ProjetGL.Models;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-
 namespace ProjetGL.Data
 {
     public class Gestion_Account : IGestion_Account
-    {
+	{
+		
+        
         public Gestion_Account()
-        {
-            Account.cp = GetMaxIdUser();
-        }
+		{
+			
+		}
+		public void UpdateAccount(string Username, Account newAccount)
+		{
+			GlobalBD.Bd.Command.CommandText = @"UPDATE Compte SET password = '" + newAccount.Password + "', role = '" + newAccount.Role +"' WHERE login = '" + Username + "'";
 
-
-        public void Add(Account account)
-        {
-            GlobalBD.Bd.Command.CommandText = $@"insert into account values('{account.Id}','{account.Username}','{account.Password}','{account.UserRole}')";
-            GlobalBD.Bd.Command.ExecuteNonQuery();
-        }
-
-        public void Del(string login)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Exist(string username)
-        {
-			GlobalBD.Bd.Command.CommandText = $@"select * from account where Username ='{username}'";
-            SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
-            if (rd.Read())
-            {
-                rd.Close();
-                return true;
-            }
-            rd.Close();
-            return false;
-        }
-
-        public Account Find(string Username)
-        {
-			GlobalBD.Bd.Command.CommandText = $@"select * from account where Username ='{Username}'";
-            SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
-            if (rd.Read())
-            {
-                var account = new Account();
-
-                account.Username = rd["Username"].ToString();
-                account.Password = rd["Password"].ToString();
-                account.UserRole = (Role)Enum.Parse(typeof(Role), rd["UserRole"].ToString());
-
-                rd.Close();
-                return account;
-            }
-            rd.Close();
-            return null;
-        }
-
-
-        public List<Account> GetAccounts()
-        {
-			GlobalBD.Bd.Command.CommandText = "select * from account";
-            SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
-            List<Account> accounts = new List<Account>();
-            while (rd.Read())
-            {
-                accounts.Add(new Account
-                {
-                    Username = rd["Username"].ToString(),
-                    Password = rd["Password"].ToString()
-                });
-            }
-            rd.Close();
-            return accounts;
-        }
-
-        public void Update(string Username, Account newAccount)
-        {
-			GlobalBD.Bd.Command.CommandText = $@"update account set Username = '{newAccount.Username}', Password = '{newAccount.Password}' where Username = '{Username}'";
 			GlobalBD.Bd.Command.ExecuteNonQuery();
-        }
+		}
 
 
-        public int GetMaxIdUser()
-        {
-            try
-            {
-                int maxId = 0;
-				// Requête pour récupérer le maximum de IdUser
-				GlobalBD.Bd.Command.CommandText = $@"SELECT ISNULL(MAX(Id), 0) FROM account";
 
-                SqlDataReader rs = GlobalBD.Bd.Command.ExecuteReader();
-                if (rs.Read() == true)
-                {
-                    maxId = (int)rs[0];
-                    rs.Close();
-                }
-                return maxId;
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erreur lors de la récupération du MaxIdUser : {ex.Message}");
-                return 0; // Retourne 0 en cas d'erreur
-            }
-        }
+		public void AddAccount(Account account)
+		{
+			GlobalBD.Bd.Command.CommandText = $@"INSERT INTO Compte (login, password, role) 
+                                         VALUES ('{account.Username}', '{account.Password}', '{account.Role}')";
+			GlobalBD.Bd.Command.ExecuteNonQuery();
+		}
 
 
-    }
+
+		public bool Exists(string username)
+		{
+			GlobalBD.Bd.Command.CommandText = $@"SELECT * FROM Compte WHERE login = '{username}'";
+			SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
+
+			if (rd.Read())
+			{
+				rd.Close();
+				return true;
+			}
+
+			rd.Close();
+			return false;
+		}
+
+
+
+		public Account FindAccount(string Username)
+		{
+			GlobalBD.Bd.Command.CommandText = $@"SELECT * FROM Compte WHERE login = '{Username}'";
+			SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
+
+			if (rd.Read())
+			{
+				var account = new Account();
+
+				account.Username = rd["login"].ToString();
+				account.Password = rd["password"].ToString();
+				account.Role = rd["role"].ToString();
+
+				rd.Close();
+				return account;
+			}
+
+			rd.Close();
+			return null;
+		}
+
+
+		public void DeleteAccount(string username)
+		{
+			GlobalBD.Bd.Command.CommandText = $@"DELETE FROM Compte WHERE login = '{username}'";
+			GlobalBD.Bd.Command.ExecuteNonQuery();
+		}
+
+
+
+		public List<Account> GetAllAccounts()
+		{
+			GlobalBD.Bd.Command.CommandText = "SELECT * FROM Compte";
+			SqlDataReader rd = GlobalBD.Bd.Command.ExecuteReader();
+
+			List<Account> accounts = new List<Account>();
+
+			while (rd.Read())
+			{
+				accounts.Add(new Account
+				{
+					Username = rd["login"].ToString(),
+					Password = rd["password"].ToString(),
+					Role = rd["role"].ToString()
+				});
+			}
+
+			rd.Close();
+			return accounts;
+		}
+
+
+
+
+
+
+
+
+	}
+
 }
+    
---
+++ ProjetGL/Data/Gestion_AppelOffre.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Data.SqlClient;
+using ProjetGL.Models;
+using ProjetGL.GlobalServices;
+using ProjetGL.Interfaces;
+
+namespace ProjetGL.Data
+{
+    public class Gestion_AppelOffre : IGestion_AppelOffre
+	{
+        public Gestion_AppelOffre()
+        {
+            
+        }
+		public void AddAppelOffre(AppelOffre appel, List<int> ressourcesIds)
+		{
+			GlobalBD.Bd.Command.CommandText = $@"
+            INSERT INTO AppelDoffre (DateDebut, DateFin, Description) 
+            VALUES ('{appel.DateDebut}', '{appel.DateFin}', '{appel.Description}');
+            SELECT SCOPE_IDENTITY();";
+
+			int nouvelAppelId = Convert.ToInt32(GlobalBD.Bd.Command.ExecuteScalar());
+
+			foreach (var ressourceId in ressourcesIds)
+			{
+				GlobalBD.Bd.Command.CommandText = $@"
+                INSERT INTO AppelDoffre_Ressources (AppelId, RessourceId) 
+                VALUES ('{nouvelAppelId}', '{ressourceId}')";
+
+				GlobalBD.Bd.Command.ExecuteNonQuery();
+			}
+		}
+
+
+		public void DeleteAppelOffre(int appelId)
+		{
+			// Supprimer les ressources associées
+			GlobalBD.Bd.Command.CommandText = $@"DELETE FROM AppelDoffre_Ressources WHERE AppelId = '{appelId}'";
+			GlobalBD.Bd.Command.ExecuteNonQuery();
+
+			// Supprimer l'appel d'offre lui-même
+			GlobalBD.Bd.Command.CommandText = $@"DELETE FROM AppelDoffre WHERE AppelId = '{appelId}'";
+			GlobalBD.Bd.Command.ExecuteNonQuery();
+		}
+
+
+		public List<AppelOffre> GetAllAppelOffre()
+		{
+			List<AppelOffre> appelsOffres = new List<AppelOffre>();
+
+			GlobalBD.Bd.Command.CommandText = "SELECT * FROM AppelDoffre";
+			SqlDataReader reader = GlobalBD.Bd.Command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				AppelOffre appel = new AppelOffre
+				{
+					AppelId = Convert.ToInt32(reader["AppelId"]),
+					DateDebut = Convert.ToDateTime(reader["DateDebut"]),
+					DateFin = Convert.ToDateTime(reader["DateFin"]),
+					Description = reader["Description"].ToString(),
+					Ressources = new List<Ressource>()
+				};
+
+				appelsOffres.Add(appel);
+			}
+
+			reader.Close();
+
+			// Charger les ressources associées
+			foreach (var appel in appelsOffres)
+			{
+				appel.Ressources = GetRessourcesByAppelId(appel.AppelId);
+			}
+
+			return appelsOffres;
+		}
+
+
+		public AppelOffre FindAppelOffre(int appelId)
+		{
+			AppelOffre appel = null;
+
+			GlobalBD.Bd.Command.CommandText = $@"SELECT * FROM AppelDoffre WHERE AppelId = '{appelId}'";
+			SqlDataReader reader = GlobalBD.Bd.Command.ExecuteReader();
+
+			if (reader.Read())
+			{
+				appel = new AppelOffre
+				{
+					AppelId = Convert.ToInt32(reader["AppelId"]),
+					DateDebut = Convert.ToDateTime(reader["DateDebut"]),
+					DateFin = Convert.ToDateTime(reader["DateFin"]),
+					Description = reader["Description"].ToString(),
+					Ressources = new List<Ressource>() // Initialisation de la liste
+				};
+			}
+
+			reader.Close();
+
+			// Charger les ressources associées
+			if (appel != null)
+			{
+				appel.Ressources = GetRessourcesByAppelId(appelId); // ✅ Charger les ressources associées
+			}
+
+			return appel;
+		}
+
+
+		public List<Ressource> GetRessourcesByAppelId(int appelId)
+		{
+			List<Ressource> ressources = null;
+
+			GlobalBD.Bd.Command.CommandText = @$"SELECT r.* FROM Ressources r INNER JOIN AppelDoffre_Ressources ar ON r.RessourceId = ar.RessourceId WHERE ar.AppelId = '{appelId}'";
+			GlobalBD.Bd.Command.Parameters.Clear();
+
+			SqlDataReader reader = GlobalBD.Bd.Command.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				ressources = new List<Ressource>();
+				while (reader.Read())
+				{
+					Ressource ressource = new Ressource
+					{
+						RessourceId = Convert.ToInt32(reader["RessourceId"]),
+						Type = reader["Type"].ToString(),
+						Marque = reader.IsDBNull(reader.GetOrdinal("Marque")) ? null : reader["Marque"].ToString(),
+						RAM = reader.IsDBNull(reader.GetOrdinal("RAM")) ? (int?)null : Convert.ToInt32(reader["RAM"]),
+						CPU = reader.IsDBNull(reader.GetOrdinal("CPU")) ? null : reader["CPU"].ToString(),
+						DisqueDur = reader.IsDBNull(reader.GetOrdinal("DisqueDur")) ? null : reader["DisqueDur"].ToString(),
+						Ecran = reader.IsDBNull(reader.GetOrdinal("Ecran")) ? null : reader["Ecran"].ToString(),
+						VitesseImpression = reader.IsDBNull(reader.GetOrdinal("VitesseImpression")) ? null : reader["VitesseImpression"].ToString(),
+						Resolution = reader.IsDBNull(reader.GetOrdinal("Resolution")) ? null : reader["Resolution"].ToString()
+					};
+
+					ressources.Add(ressource);
+				}
+			}
+
+			reader.Close();
+			return ressources;
+		}
+
+		public List<Ressource> GetRessourcesByAppelOffre(int appelId)
+		{
+			List<Ressource> ressources = new List<Ressource>();
+
+			GlobalBD.Bd.Command.CommandText = $@"SELECT r.RessourceId, r.Type, r.Marque FROM Ressources r JOIN AppelDoffre_Ressources ar ON r.RessourceId = ar.RessourceId WHERE ar.AppelId = {appelId}";
+
+			SqlDataReader reader = GlobalBD.Bd.Command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Ressource ressource = new Ressource
+				{
+					RessourceId = reader.GetInt32(reader.GetOrdinal("RessourceId")),
+					Type = reader.GetString(reader.GetOrdinal("Type")),
+					Marque = reader.IsDBNull(reader.GetOrdinal("Marque")) ? null : reader.GetString(reader.GetOrdinal("Marque"))
+				};
+				ressources.Add(ressource);
+			}
+
+			reader.Close();
+			return ressources;
+		}
+
+
+		
+    }
+}
