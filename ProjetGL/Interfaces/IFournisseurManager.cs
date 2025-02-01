9f93b351231dcc83542e268da47362528a92391e
OussamaAatany <o.aatany@esisa.ac.ma>
2025-02-01 17:58:25 +0100

    dd

---
+++ ProjetGL/Interfaces/IFournisseurManager.cs
@@ -0,0 +1,12 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+    public interface IFournisseurManager
+    {
+        void AddFournisseur(Fournisseur fournisseur);
+        Fournisseur FindFournisseur(string email);
+        void UpdateFournisseur(Fournisseur fournisseur, string Email);
+        List<Fournisseur> GetAllFournisseurs();
+    }
+}
