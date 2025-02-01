25c2abb2f16563a8ba459251d9a423353274afa7
OussamaAatany <o.aatany@esisa.ac.ma>
2025-02-01 17:55:26 +0100

    ff

---
+++ ProjetGL/Business/PropositionManager.cs
@@ -0,0 +1,30 @@
+﻿using ProjetGL.Data;
+using ProjetGL.Interfaces;
+using ProjetGL.Models;
+
+namespace ProjetGL.Business
+{
+    public class PropositionManager : IPropositionManager
+    {
+        IGestion_Proposition data = new Gestion_Proposition();
+        public void AddProposition(Proposition proposition)
+        {
+            if (data.FindProposition(proposition.PropositionId) == null)
+                data.AddProposition(proposition);
+            else
+                throw new Exception("proposition already exist");
+        }
+
+
+        public Proposition FindProposition(int id)
+        {
+            return data.FindProposition(id);
+        }
+
+        public List<Proposition> GetAllPropositions()
+        {
+            return data.GetAllPropositions();
+        }
+
+    }
+}
