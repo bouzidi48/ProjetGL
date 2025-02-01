db5090533180d28b461c6b1c3d0999cf24240903
Ilyass <i.haddad@esisa.ac.ma>
2025-02-01 18:06:08 +0100

    dd

--- ProjetGL/Models/Fournisseur.cs
+++ ProjetGL/Models/Fournisseur.cs
@@ -1,35 +1,30 @@
 ﻿namespace ProjetGL.Models
 {
-	public class Fournisseur:Account
-	{
-		private static int cp = 0;
-		private int id;
-		private string lieu;
-		private string adresse;
-		private string siteInternet;
-		private string gerant;
-		private int blackListe;
-		private string motif;
+    public class Fournisseur
+    {
+        private string email;
+        private string password;
+        private string fournisseurName;
+        private string nomSociete;
+        private string gerant;
+        private string status;
 
+        public Fournisseur()
+        {
+            Email = "Erreur@gmail.com";
+            Password = "";
+            FournisseurName = "Erreur";
+            NomSociete = "Erreur";
+            Gerant = "Erreur";
+            Status = "Non";
 
-		public int Id { get => id; set => id = value; }
-		public string Lieu { get => lieu; set => lieu = value; }
-		public string Adresse { get => adresse; set => adresse = value; }
-		public string SiteInternet { get => siteInternet; set => siteInternet = value; }
-		public string Gerant { get => gerant; set => gerant = value; }
-		public int BlackListe { get => blackListe; set => blackListe = value; }
-		public string Motif { get => motif; set => motif = value; }
+        }
 
-		public Fournisseur(string username, string password, Role userRole, string lieu, string adresse, string siteInternet, string gerant, int blackListe, string motif) : base(username, password, userRole)
-		{
-			cp++;
-			Id = cp;
-			Lieu = lieu;
-			Adresse = adresse;
-			SiteInternet = siteInternet;
-			Gerant = gerant;
-			BlackListe = blackListe;
-			Motif = motif;
-		}
-	}
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string FournisseurName { get => fournisseurName; set => fournisseurName = value; }
+        public string NomSociete { get => nomSociete; set => nomSociete = value; }
+        public string Gerant { get => gerant; set => gerant = value; }
+        public string Status { get => status; set => status = value; }
+    }
 }
