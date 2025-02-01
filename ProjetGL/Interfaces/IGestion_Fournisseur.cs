7ad7da16740d6bd6ecfa17921548c037946adf5c
Ilyass <i.haddad@esisa.ac.ma>
2025-02-01 18:02:24 +0100

    hh

---
+++ ProjetGL/Interfaces/IGestion_Fournisseur.cs
@@ -0,0 +1,12 @@
+﻿using ProjetGL.Models;
+
+namespace ProjetGL.Interfaces
+{
+    public interface IGestion_Fournisseur
+    {
+        void AddFournisseur(Fournisseur fournisseur);
+        Fournisseur FindFournisseur(string email);
+        void UpdateFournisseur(Fournisseur fournisseur, string Email);
+        List<Fournisseur> GetAllFournisseurs();
+    }
+}
